cc00a27b50bd467ff7fefbc55089581aaa577aaf
nicosammito <nico@sammito.de>
2023-08-07 09:43:20 +0000

    Deploying to gh-pages from @ nicosammito/GLS@2ea1da9552a9af0ca0cdb0c19d782d5219709618 🚀

--- _next/static/A0DLQfnD9DRfJZYUcAHX8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fa34affa7fa8d177.css","static/chunks/pages/index-462060d4cb968efc.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
